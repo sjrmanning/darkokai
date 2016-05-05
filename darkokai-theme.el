8b770069546945745202dbf9fb8b75601c3586fc
Simon Manning <simon@ecksd.com>
2016-05-05 23:28:19 +0800

    More summary update.

--- darkokai-theme.el
+++ darkokai-theme.el
@@ -1,5 +1,21 @@
 ;;; darkokai-theme.el --- A darker variant on Monokai.
+
+;; URL: http://github.com/sjrmanning/darkokai
+;; Version: 0.1.0
+
 ;;; Commentary:
+;;
+;; This theme and particular the structure of this file is entirely based
+;; on the great work by `oneKelvinSmith' and his port of the monokai theme
+;; found at https://github.com/oneKelvinSmith/monokai-emacs.
+;;
+;; Darkokai arose after I found myself making more and more small tweaks
+;; to monokai via a separate theme overrides file. Eventually it made more
+;; sense to create a new theme as it was diverging significantly from the
+;; original theme.
+;;
+;; Pull requests, and suggestions are most welcome!
+;;
 ;;; Code:
 
 (unless (>= emacs-major-version 24)
